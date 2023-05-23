ddb138acf513640546a276597ae46ed88bd893f7
samueljustus <113977877+samueljustus@users.noreply.github.com>
2023-05-23 06:57:25 +0100

    project placed on hold

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#mail');
+const password = document.querySelector('#password');
+const errorMessage = document.querySelectorAll('.error')
